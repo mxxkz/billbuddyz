3a4bdbcf5004ff4d7f77fb34eabd0067a3a381b3
mxxkz <jbmook@outlook.co.th>
2024-02-29 15:56:49 +0700

    setup project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withSerwistInit from '@serwist/next'
 
-export default nextConfig;
+const withSerwist = withSerwistInit({
+    cacheOnFrontEndNav: true,
+    swSrc: 'src/utils/sw.ts', // add the path where you create sw.ts
+    swDest: 'public/sw.js',
+    reloadOnOnline: true,
+    // disable: process.env.NODE_ENV === "development", // to disable pwa in development
+    disable: false
+})
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    swcMinify: true,
+    reactStrictMode: true,
+}
+
+export default withSerwist(nextConfig)
